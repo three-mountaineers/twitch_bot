24bac6b214351eff67b19d83b996d2510abf70e4
Three Mountaineers <three.mountaineers3@gmail.com>
2024-05-19 00:26:25 +1000

    .

---
+++ mountaineer_bot/components/sound_reactor.py
@@ -0,0 +1,18 @@
+from typing import Optional, Dict
+import logging
+
+from twitchio.ext import commands, routines
+from twitchio.message import Message
+
+from mountaineer_bot import BotMixin
+from mountaineer_bot.security import restrict_command
+
+class SoundReactor(BotMixin):
+    _required_scope = [
+        'chat:read',
+    ]
+
+    async def event_message(self, message: Message):
+        await super().event_message(message=message)
+        logging.log(logging.DEBUG, f'[{message.channel.name}] {message.author.name}: {message.content}')
+        pass
