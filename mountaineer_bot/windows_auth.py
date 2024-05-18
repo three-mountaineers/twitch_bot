7f9a70c27f6e1fec5e5b7724c42b54af6f9023ea
Three Mountaineers <three.mountaineers3@gmail.com>
2024-05-18 20:29:30 +1000

    Add linux support for auth

--- mountaineer_bot/windows_auth.py
+++ mountaineer_bot/windows_auth.py
@@ -1,5 +1,34 @@
 import configparser
-import keyring
+from typing import Optional
+import platform
+import os
+if platform.system() == 'Windows':
+    import keyring
+elif platform.system() == 'Linux':
+    import stat
+    class Keyring:
+        def __init__(self):
+            self._file_path = os.path.join(os.path.expanduser('~'), '.config/twitch_bot.cfg')
+            if not os.path.isdir(os.path.split(self._file_path)[0]):
+                os.makedirs(os.path.split(self._file_path)[0])
+            if not os.path.isfile(self._file_path):
+                with open(self._file_path, 'w') as f:
+                    f.write('')
+            os.chmod(self._file_path, ((stat.S_IWUSR & (~stat.S_IRGRP) & (~stat.S_IROTH)) | stat.S_IRUSR))
+        def get_password(self, service_name: str, username: str) -> Optional[str]:
+            cfg = configparser.ConfigParser()
+            cfg.read(self._file_path)
+            return cfg[service_name][username]
+
+        def set_password(self, service_name: str, username: str, password: str):
+            cfg = configparser.ConfigParser()
+            cfg.read(self._file_path)
+            if service_name not in cfg:
+                cfg.add_section(service_name)
+            cfg.set(service_name, username, password)
+            with open(self._file_path, 'w') as f:
+                cfg.write(f)
+    keyring = Keyring()
 
 def read_config(cfg_location):
     config = configparser.ConfigParser()
