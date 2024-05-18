c386afef9fb49ebd3229f204e2ddd78881055c96
Three Mountaineers <three.mountaineers3@gmail.com>
2024-05-19 00:25:20 +1000

    Avoid circular import.

--- mountaineer_bot/core.py
+++ mountaineer_bot/core.py
@@ -6,7 +6,37 @@ from twitchio.ext import commands
 
 from mountaineer_bot import windows_auth
 from mountaineer_bot.twitchauth import core as twitch_auth_core
-from mountaineer_bot.security import restrict_command
+
+from typing import Literal, List
+from functools import wraps
+
+from mountaineer_bot import core
+
+def restrict_command(
+        allowed: List[Literal['Whitelist', 'Broadcaster', 'Mods']]=['Broadcaster'], 
+        default=False, 
+        blacklist_enabled=True
+    ):
+    def decorator(func):
+        @wraps(func)
+        async def wrapper(self: "Bot", ctx, *args, **kwargs):
+            if 'Broadcaster' in allowed and ctx.author.is_broadcaster:
+                is_allowed = True
+            elif blacklist_enabled and ctx.author.name in self._bot_blacklist:
+                is_allowed = False
+            elif 'Mods' in allowed and ctx.author.is_mod:
+                is_allowed = True
+            elif 'Whitelist' in allowed and ctx.author.name in self._bot_whitelist:
+                is_allowed = True
+            else:
+                is_allowed = default
+            if not is_allowed:
+                if self._no_permission_response is not None:
+                    await self.send(ctx, self._no_permission_response)
+            else:
+                await func(self, ctx, *args, **kwargs)
+        return wrapper
+    return decorator
 
 class BotMixin:
     _required_scope: List[str] = []
